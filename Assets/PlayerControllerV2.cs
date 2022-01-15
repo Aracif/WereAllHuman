10e54107b786d2e42eb80c051b5247ab27131c59
Salvatore Ficara <ficaras0@gmail.com>
2022-01-15 16:32:46 -0500

    Cleaned up code, replace boxcollider with capsule collider

--- Assets/PlayerControllerV2.cs
+++ Assets/PlayerControllerV2.cs
@@ -5,7 +5,8 @@ public class PlayerControllerV2 : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rigidbody2D;
-    private BoxCollider2D boxCollider2D;
+    //private BoxCollider2D boxCollider2D;
+    private CapsuleCollider2D boxCollider2D;
     private CircleCollider2D headCollider;
     public LayerMask whatIsGround;
     public LayerMask whatIsCeiling;
@@ -26,9 +27,8 @@ public class PlayerControllerV2 : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponentInParent<Rigidbody2D>();
-        boxCollider2D = GetComponentInParent<BoxCollider2D>();
+        boxCollider2D = GetComponentInParent<CapsuleCollider2D>();
         headCollider = transform.Find("Root Bone").GetChild(0).GetChild(0).GetComponent<CircleCollider2D>();
-        print(headCollider);
     }
 
     private void FixedUpdate()
@@ -62,18 +62,32 @@ public class PlayerControllerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Check if you hit something while moving upwards
         bool hitHead = this.hitHead; ;
-
         this.hitHead = sendHeadRay().collider != null;
         if (this.hitHead)
         {
             rigidbody2D.isKinematic = false;
-            print("noggin knocker");
         }
 
+        //Check if youre grounded
+        bool wasGrounded = grounded;
+        grounded = Physics2D.OverlapCircle(boxCollider2D.gameObject.transform.position, .1f, whatIsGround);
+
+        if (!wasGrounded && grounded)
+        {
+            rigidbody2D.isKinematic = false;
+            animator.SetBool("jump", false);
+            animator.SetBool("grounded", true);
+        }
+        else if (!grounded)
+        {
+            animator.SetBool("grounded", false);
+        }
+
+        //Locomotion
         rawSpeed = Input.GetAxis("Horizontal");
         animator.SetFloat("speed", Mathf.Abs(rawSpeed));
-        playerGroundRayHit = sendGroundRay().collider != null;
 
         if (rawSpeed < 0.0 && !m_FacingRight)
         {
@@ -83,72 +97,45 @@ public class PlayerControllerV2 : MonoBehaviour
         {
             Flip();
         }
+
         jumpInput();
 
     }
 
+    //Used as animation event for standard single jump
     public void setKinematic(string val)
     {
         isKinematic = bool.Parse(val);
         rigidbody2D.isKinematic = isKinematic;
         animator.SetBool("falling", true);
     }
+
     void jumpInput() 
     {
-
-        bool wasGrounded = grounded;
-        grounded = Physics2D.OverlapCircle(boxCollider2D.gameObject.transform.position, .1f, whatIsGround);
-
         if (Input.GetButtonDown("Jump") && grounded)
         {
             rigidbody2D.isKinematic = true;
             animator.SetBool("jump", true);
             animator.SetTrigger("jumpTrigger");
-        }
-        
-        if (!wasGrounded && grounded)
-        {
-            animator.SetBool("grounded", true);
-            
-            if (animator.GetBool("jump"))
-            {
-                animator.SetBool("jump", false);
-            }
-        }
-        else if (wasGrounded && !grounded)
-        {
-            animator.SetBool("grounded", false);
+        }             
+    }
 
-        }
 
-        if (groundedCounter <= 0)
+    //Recall that when this method is implemented you will receive "Root motion handled by script"
+    //in unity IDE.
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+        if (animator)
         {
-
+            transform.parent.position += animator.deltaPosition;
+            transform.parent.rotation = animator.deltaRotation;
         }
-        else
-        {
-            groundedCounter -= Time.deltaTime;
-        }
-        
     }
 
     bool isFalling()
     {
         return rigidbody2D.velocity.y < -1f;
-    }
-
-    void OnAnimatorMove()
-    {
-        Animator animator = GetComponent<Animator>();
-        //print("animator move");
-        if (animator)
-        {
-            //Vector3 newPosition = transform.position;
-            //newPosition.x += animator.GetFloat("Runspeed") * Time.deltaTime;
-            //transform.position = newPosition;
-            transform.parent.position += animator.deltaPosition;
-            transform.parent.rotation = animator.deltaRotation;
-        }
     }
 
     RaycastHit2D sendGroundRay()
@@ -164,7 +151,6 @@ public class PlayerControllerV2 : MonoBehaviour
         {
             rayColor = Color.red;
         }
-        //print("Hit: " + raycastHit.collider?.gameObject?.name);
         Debug.DrawRay(boxCollider2D.bounds.center, Vector2.down * (boxCollider2D.bounds.extents.y), rayColor);
         return raycastHit;
     }    
@@ -182,17 +168,14 @@ public class PlayerControllerV2 : MonoBehaviour
         {
             rayColor = Color.red;
         }
-        //print("Hit: " + raycastHit.collider?.gameObject?.name);
         Debug.DrawRay(headCollider.bounds.center, Vector2.up * (headCollider.bounds.extents.y), rayColor);
         return raycastHit;
     }
 
     private void Flip()
     {
-        // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
 
-        // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
